30508336e7dbf1207d0fbd8416e970851a3e14fa
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-09-01 14:17:17 +0900

    Add location.pathname

--- PrivateRoute.js
+++ PrivateRoute.js
@@ -5,16 +5,21 @@ const e = React.createElement
 
 const PrivateRoute = ({
   redirectPath = '/login',
+  withReturnUrl = true,
+  returnUrlName = 'returnUrl'
   checkAuthenticated,
   component,
   ...rest
 }) =>
   e(Route, {
     ...rest,
-    render: props =>
-      checkAuthenticated()
-        ? e(component, props)
-        : e(Redirect, { to: { pathname: redirectPath, state: { returnUrl: 'TODO' } } }),
+    render: props => {
+      const to = { pathname: redirectPath }
+      if (withReturnUrl) {
+        to.search = `?${returnUrlName}=${encodeURIComponent(props.location.pathname)}`
+      }
+      return checkAuthenticated() ? e(component, props) : e(Redirect, { to })
+    },
   })
 
 module.exports = PrivateRoute
